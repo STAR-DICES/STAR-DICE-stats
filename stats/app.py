5b693aa213cdcd8b01e1d92f9217b2c3f875c9d1
uniGio <55856091+uniGio@users.noreply.github.com>
2019-11-24 22:08:18 +0100

    correct a line in app.py

--- stats/app.py
+++ stats/app.py
@@ -12,7 +12,7 @@ def start(test = False):
     app = create_app(blueprints=blueprints)
     if test:
         app.config['TESTING'] = True
-    api_doc(app, config_path='follows-specs.yaml', url_prefix='/api', title='API doc')
+    api_doc(app, config_path='stats-specs.yaml', url_prefix='/api', title='API doc')
     return app
 
 if __name__ == '__main__':
